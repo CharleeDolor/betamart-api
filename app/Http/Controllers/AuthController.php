cafa63f0a6eec0a2838aac0b174312d35ccdccdc
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-05-25 18:15:47 +0800

    deleted unnecessary comments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,27 +24,6 @@ class AuthController extends Controller
             $user = User::where('email', $data['email'])->first();
             $token = $user->createToken('user')->plainTextToken;
             return response()->json(['token' => $token], 200);
-
-            // $request->validate([
-            //     'email' => 'required|email',
-            //     'password' => 'required|string',
-            // ]);
-
-            // $user_data = array(
-            //     'email'  => $request->get('email'),
-            //     'password' => $request->get('password')
-            // );
-
-            // if(Auth::attempt($user_data)){
-            //     /** @var \App\Models\User $user **/
-            //     $user = Auth::user();
-            //     $token = $user->createToken('user')->plainTextToken;
-
-            //     return response()->json(['token' => $token], 200);
-            // } else {
-
-            //     return response()->json(['message' => 'Invalid username or password'], 200);
-            // }
         } catch (\Throwable $th) {
             return response()->json(['message' => $th], 500);
         }
